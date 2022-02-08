c728e56209aaede1ee9c441fd4f76f15157ec266
Song Y <65588098+richeberry@users.noreply.github.com>
2022-02-08 21:48:51 +0900

    Delete 10809.py

--- 10809.py
+++
@@ -1,9 +0,0 @@
-
-s = input()
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-
-for i in alpha:
-    if i in s:
-        print(s.index(i), end=' ') # idx 는 안됨 / 줄 바꾸기가 아니고 띄어쓰기로 출력
-    else:
-        print(-1, end=' ') 
