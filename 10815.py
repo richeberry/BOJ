daa7f04399d1636197ecabee586837001aff0d08
richeberry <richeberry@gmail.com>
2022-06-02 23:54:58 +0900

    BOJ

---
+++ 10815.py
@@ -0,0 +1,13 @@
+n = int(input())
+cardlst = list(map(int, input().split()))
+m = int(input())
+tellcard = list(map(int, input().split()))
+printlst = []
+for i in tellcard:
+    if i in cardlst:
+        printlst.append(1)
+    else:
+        printlst.append(0)
+
+for i in printlst:
+    print(i, end=' ')
