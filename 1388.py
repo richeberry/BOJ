2322d7f7f5bcefddc7862208d903a9b75e8f1756
richeberry <richeberry@gmail.com>
2022-07-28 22:08:24 +0900

    BFS

---
+++ 1388.py
@@ -0,0 +1,20 @@
+# 1388
+
+from collections import deque
+
+# graph = []
+# n, m = map(int, input().split())
+# for i in range(n):
+#     graph.append(list(input()))
+
+graph = [['-', '|', '|', '-', '-', '|'], ['|', '|', '|', '|', '|', '|'], ['|', '|', '|', '-', '|', '-'], ['-', '|', '|', '|', '|', '-'], ['|', '|', '|', '|', '-', '|'], ['|', '|', '-', '|', '|', '-']]
+print(graph)
+
+def bfs(graph, start, visit):
+    que = deque([start])
+    visit[start] = True
+    while que:
+        v = que.popleft()
+        for i in graph[v]:
+            for j in i:
+                if j == 
