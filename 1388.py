e289a7ac10a8541dd5ed6846ccdd778ca0975333
richeberry <richeberry@gmail.com>
2022-07-31 23:36:30 +0900

    BFS

--- 1388.py
+++ 1388.py
@@ -18,3 +18,23 @@ def bfs(graph, start, visit):
         for i in graph[v]:
             for j in i:
                 if j == 
+
+
+from collections import deque
+
+# graph = []
+# n, m = map(int, input().split())
+# for i in range(n):
+#     graph.append(list(input()))
+
+graph = [['-', '|', '|', '-', '-', '|'], ['|', '|', '|', '|', '|', '|'], ['|', '|', '|', '-', '|', '-'], ['-', '|', '|', '|', '|', '-'], ['|', '|', '|', '|', '-', '|'], ['|', '|', '-', '|', '|', '-']]
+print(graph)
+
+def bfs(graph, start, visit):
+    que = deque([start])
+    visit[start] = True
+    while que:
+        v = que.popleft()
+        for i in graph[v]:
+            for j in i:
+                if j == 
