40c71560478c0181ab2acc8c540f2d1bfe4eff32
richeberry <richeberry@gmail.com>
2022-08-02 20:57:41 +0900

    -

--- yet_11729.py
+++ yet_11729.py
@@ -21,3 +21,45 @@ print(hanoi(n,a,b,c))
 
 # for i in a:
 #     print(i)
+
+
+# 1388
+
+from collections import deque
+
+# graph = []
+# n, m = map(int, input().split())
+# for i in range(n):
+#     graph.append(list(input()))
+
+graph = [['-', '|', '|', '-', '-', '|'], ['|', '|', '|', '|', '|', '|'], ['|', '|', '|', '-', '|', '-'], ['-', '|', '|', '|', '|', '-'], ['|', '|', '|', '|', '-', '|'], ['|', '|', '-', '|', '|', '-']]
+print(graph)
+
+def bfs(graph, start, visit):
+    que = deque([start])
+    visit[start] = True
+    while que:
+        v = que.popleft()
+        for i in graph[v]:
+            for j in i:
+                if j == 
+
+
+from collections import deque
+
+# graph = []
+# n, m = map(int, input().split())
+# for i in range(n):
+#     graph.append(list(input()))
+
+graph = [['-', '|', '|', '-', '-', '|'], ['|', '|', '|', '|', '|', '|'], ['|', '|', '|', '-', '|', '-'], ['-', '|', '|', '|', '|', '-'], ['|', '|', '|', '|', '-', '|'], ['|', '|', '-', '|', '|', '-']]
+print(graph)
+
+def bfs(graph, start, visit):
+    que = deque([start])
+    visit[start] = True
+    while que:
+        v = que.popleft()
+        for i in graph[v]:
+            for j in i:
+                if j == 
