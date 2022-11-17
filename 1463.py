48d5cb468af7f44c886828d2e7522142d88415ff
richeberry <richeberry@gmail.com>
2022-11-17 23:01:17 +0900

    221117

--- 1463.py
+++ 1463.py
@@ -1,33 +1,16 @@
 
 # BOJ _ 1463 1로 만들기
 
-n = 5
+x = int(input())
 
-dp = [0] * (n + 1)
-dp[1] = 1
+dp = [0] * (x + 1)
 
-# bottom up으로 진행
+for i in range(2, x+1):
+    dp[i] = dp[i-1] + 1 # 2와 3으로 나누어지지 않는 숫자면 -1을 해야하기 때문에 1씩 증가시켜줌
 
-# 시작 전 DP 확인
-print(dp)
+    if i % 2 == 0: # 2로 나누어지면
+        dp[i] = min(dp[i], dp[i//2] + 1) # dp에 저장된 수와 i를 2로 나누었을 때의 숫자와 비교
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1) # dp에 저장된 수와 i를 3으로 나누었을 떄의 숫자와 비교
 
-tmp = 1
-before = dp[0]
-while True:
-    # 탈출 조건
-    print(dp)
-    print('tmp',tmp)
-    print('before',before)
-    if before == n:
-        break
-
-    elif (tmp * 2) % n == 0:
-        dp[n] = before + 1
-        before = tmp * 2
-    elif (tmp * 3) % n == 0:
-        dp[n] = before + 1
-        before = tmp * 3
-    else:
-        dp[n] = before + 1
-        before = tmp + 1
-    tmp += 1
+print(dp[x])
